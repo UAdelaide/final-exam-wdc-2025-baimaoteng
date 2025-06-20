27ace4ccf84e45c55fc036003acc2a771ce47d28
Luca <baimao.teng@student.adelaide.edu.au>
2025-06-20 14:40:13 +0000

    [automatic commit] 2025-06-20 14:40:13 UTC 1.147.9.68

--- part1/app.js
+++ part1/app.js
@@ -130,7 +130,7 @@ let db;
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
         ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
-        ((SELECT dog_id FROM Dogs WHERE name = 'Charlie'), '2025-06-11 07:00:00', 60, 'North Adelaide Park', 'open'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Charlie'), '2025-06-11 07:00:00', 60, 'Adelaide Botanic Garden', 'open'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Cooper'), '2025-06-11 16:30:00', 45, 'Victoria Square', 'open'),
         ((SELECT dog_id FROM Dogs WHERE name = 'Daisy'), '2025-06-12 10:15:00', 30, 'Elder Park', 'completed')
       `);
