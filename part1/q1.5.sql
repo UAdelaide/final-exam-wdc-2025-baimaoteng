ced047b8cbd7be37bb1405e9056f3fd00001b2d4
Luca <baimao.teng@student.adelaide.edu.au>
2025-06-20 14:38:31 +0000

    [automatic commit] 2025-06-20 14:38:31 UTC 1.147.9.68

--- part1/q1.5.sql
+++ part1/q1.5.sql
@@ -17,9 +17,16 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 -- Insert Walk Requests
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
 ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+
+-- Request for Bella
 ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
-((SELECT dog_id FROM Dogs WHERE name = 'Charlie'), '2025-06-11 07:00:00', 60, 'UPDATE WalkRequests
-SET location = 'Adelaide Botanic Garden'
-WHERE request_id = 3;', 'open'),
+
+-- Additional walk requests (3 more as requested)
+-- Request for Charlie
+((SELECT dog_id FROM Dogs WHERE name = 'Charlie'), '2025-06-11 07:00:00', 60, 'North Adelaide Park', 'open'),
+
+-- Request for Cooper
 ((SELECT dog_id FROM Dogs WHERE name = 'Cooper'), '2025-06-11 16:30:00', 45, 'Victoria Square', 'open'),
+
+-- Request for Daisy
 ((SELECT dog_id FROM Dogs WHERE name = 'Daisy'), '2025-06-12 10:15:00', 30, 'Elder Park', 'completed');
