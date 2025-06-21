2f21a1be8e601b3b83ea3c354fa33bb060bdf3cf
Luca <baimao.teng@student.adelaide.edu.au>
2025-06-21 01:51:07 +0000

    [automatic commit] 2025-06-21 01:51:07 UTC 1.147.9.68

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -61,4 +61,14 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// POST logout
+router.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ error: 'Logout failed' });
+    }
+    res.json({ message: 'Logged out successfully' });
+  });
+});
+
 module.exports = router;
